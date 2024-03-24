b74323e3b75cae2e0d79c5b7ab8755f5c09fcf8a
MaiJiaHong <2360655509@qq.com>
2024-03-24 17:15:55 +0800

    commit all

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setup(
     name='scPANTHEON',# 需要打包的名字,即本模块要发布的名字
-    version='0.4.0.8',#版本
+    version='0.4.0.9',#版本
     description='A graphical interface for single cell analysis.', # 简要描述
     packages=['scpantheon', 'scpantheon.app', 'scpantheon.front_end'],   #  需要打包的模块
     author='xinzhu', # 作者名
